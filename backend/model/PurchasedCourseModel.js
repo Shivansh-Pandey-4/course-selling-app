3d0bcccd951a0fe8a7f8453462bff93a23ac1aa1
Shivansh-Pandey-4 <shivanshofficial8750@gmail.com>
2025-07-12 09:22:12 +0530

    add purchased model & purchase course  route

---
+++ backend/model/PurchasedCourseModel.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose");
+
+const purchasedCourseSchema = new mongoose.Schema({
+      course_id : {
+          type : mongoose.Schema.Types.ObjectId,
+          required : true,
+          ref : "Course"
+      },
+      user_id : {
+         type : mongoose.Schema.Types.ObjectId,
+         required : true,
+         ref : "User"
+      },
+      author_id : {
+         type : mongoose.Schema.Types.ObjectId,
+         required : true,
+         ref : "Admin"
+      }
+},{timestamps:true});
+
+        const PurchasedCourseModel = mongoose.model("PurchasedCourse",purchasedCourseSchema);
+
+module.exports = PurchasedCourseModel;
--- backend/routes/courseRoutes.js
+++ backend/routes/courseRoutes.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const router = express.Router();
 const CourseModel = require("../model/CourseModel");
+const authMiddleware = require("../middleware/authMiddleware");
+const PurchasedCourseModel = require("../model/PurchasedCourseModel");
 
 router.get("/",async (req,res)=>{
       try{
@@ -15,6 +17,51 @@ router.get("/",async (req,res)=>{
             detailError : "Cannot /Get Courses. "+err.message
         })
       }
+});
+
+router.get("/:course_id",async(req,res)=>{
+       const {course_id} = req.params;
+
+       if(!course_id){
+           return res.status(411).send({
+              msg : "course cannot found without id"
+           })
+       }
+
+     try{
+            const courseExist = await CourseModel.findById(course_id).populate({path: "author_id", select:"name"});
+            if(!courseExist){
+                  return res.status(401).send({
+                      msg : "no course Exist with this course_id"
+                  })
+            }
+
+            return res.send({
+                  msg : "course exist",
+                  courseExist
+            })
+
+     }catch(err){
+          return res.status(500).send({
+              msg : "server issue:",
+              detailError : err.message
+          })
+     }
+})
+
+router.post("/purchase/:course_id", authMiddleware, async (req,res)=>{
+              const {course_id} = req.params;
+              if(!course_id){
+                  return res.status(411).send({
+                      msg : "course cannot found without its id"
+                  })
+              }
+              try{
+                   const courseExist = await CourseModel.findById(course_id).populate({path: "author_id", select : "name"});
+              }catch(err){
+                  
+              }
+
 })
 
 module.exports = router;
