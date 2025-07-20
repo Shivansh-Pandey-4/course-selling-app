3d23856850e390abbd679800d746b0c08ee948cd
Shivansh-Pandey-4 <shivanshofficial8750@gmail.com>
2025-07-20 10:38:09 +0530

    add search functionality to search courses

--- frontend/src/components/user/Courses.jsx
+++ frontend/src/components/user/Courses.jsx
@@ -4,12 +4,20 @@ import CourseCard from "./CourseCard";
 
 const Courses = ()=>{
     const [courses, setCourses] = useState([]);
+    const [courseCopy , setCourseCopy] = useState([]);
+    const [inputValue, setInputValue] = useState("");
 
     async function fetchData() {
          const response = await fetch("http://localhost:3000/courses");
          const data = await response.json();
-         console.log(data);
          setCourses(data.allCourse);
+         setCourseCopy(data.allCourse);
+    }
+
+    function searchHandler(event){
+          event.preventDefault();
+          setCourses(courses.filter((course)=>course.courseName.toLowerCase().includes(inputValue.toLocaleLowerCase())));
+          setInputValue("");
     }
 
     useEffect(()=>{
@@ -22,7 +30,15 @@ const Courses = ()=>{
 
      return (
             <div className="bg-[#2d2d2d]">
-              <div className="flex flex-wrap gap-20 justify-center py-20">
+                <form onSubmit={searchHandler} className="flex justify-center pt-10">
+
+                    <input type="text" className="border text-white w-4/12 rounded-xl px-2 py-2" placeholder="Search by Course Name" value={inputValue} onChange={(event)=>setInputValue(event.target.value)} />
+
+                    <button className="border text-white ml-2 px-3 py-1 rounded-xl bg-indigo-600 hover:bg-indigo-800 cursor-pointer">Search</button>
+
+                        <button onClick={()=>setCourses(courseCopy)} className="border text-white ml-2 px-3 py-2 rounded-xl bg-indigo-600 hover:bg-indigo-800 cursor-pointer">Reset Filter</button>
+                </form>
+              <div className="flex flex-wrap gap-20 justify-center py-15">
               {
                   courses.map((course)=> <CourseCard key={course._id} detail={course} />)
               }
